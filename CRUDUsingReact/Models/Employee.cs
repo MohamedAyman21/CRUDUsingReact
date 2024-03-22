bbed54a0d89a1247b6ab9fdf07b37cfdf0fbc3ee
MohamedAyman1998 <mohamedaymanali98@gmail.com>
2024-03-22 22:58:47 +0200

    Added Validation Annotation

--- CRUDUsingReact/Models/Employee.cs
+++ CRUDUsingReact/Models/Employee.cs
@@ -1,8 +1,11 @@
-﻿namespace CRUDUsingReact.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDUsingReact.Models
 {
 	public class Employee:PublicBaseClass
 	{
 		public string? Name { get; set; }
+		[Range(20,118)]
 		public int Age { get; set; }
 		public bool IsActive { get; set; }
 	}
